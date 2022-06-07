6638ca946c45252c2a7457168e80b8196d2a9f33
rayves <vo.raymond.v@outlook.com>
2022-06-08 02:16:47 +1000

    adds projects show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about', as: 'about'
   get 'journey', to: 'pages#journey', as: 'journey'
   get 'projects', to: 'pages#projects', as: 'projects'
+  get 'projects/:id', to: 'pages#project', as: 'project'
   get 'blog', to: 'pages#blog', as: 'blog'
   get 'contact', to: 'pages#contact', as: 'contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
