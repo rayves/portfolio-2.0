3a619cc0ca64d5d44f1880fdae9f34aaac6773bf
rayves <vo.raymond.v@outlook.com>
2022-06-08 19:31:11 +1000

    updates seed file | updates projects page - adds project cover image

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'journey', to: 'pages#journey', as: 'journey'
   get 'projects', to: 'pages#projects', as: 'projects'
   get 'projects/:id', to: 'pages#project', as: 'project'
-  get 'blog', to: 'pages#blog', as: 'blog'
+  # get 'blog', to: 'pages#blog', as: 'blog'
   get 'contact', to: 'pages#contact', as: 'contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,21 @@ Project.delete_all
 ActiveRecord::Base.connection.reset_pk_sequence!('projects')
 
 assets = [
-  'chips.jpg',
-  'hack.jpg',
-  'photograph.PNG'
+  'personal_portfolio_website/cover/screen1.png',
+  'personal_portfolio_website/other/screen2.png',
+  'personal_portfolio_website/other/screen3.png',
+  'personal_portfolio_website/other/screen4.png',
+  'personal_portfolio_website/other/screen5.png',
+  'feed_me/cover/screen1.png',
+  'feed_me/other/screen2.png',
+  'feed_me/other/screen3.png',
+  'feed_me/other/screen4.png',
+  'feed_me/other/screen5.png',
+  'roastxpress/cover/screen1.png',
+  'roastxpress/other/screen2.png',
+  'roastxpress/other/screen3.png',
+  'roastxpress/other/screen4.png',
+  'roastxpress/other/screen5.png'
 ]
 
 technologies = [
@@ -38,29 +50,43 @@ technologies = [
   { name: 'react',
     icon: 'devicon-react-original' },
   { name: 'sass',
-    icon: 'devicon-sass-original' }
+    icon: 'devicon-sass-original' },
+  { name: 'postgresql',
+    icon: 'devicon-postgresql-plain' },
+  { name: 'boostrap',
+    icon: 'devicon-bootstrap-plain' },
+  { name: 'heroku',
+    icon: 'devicon-heroku-original' },
+  { name: 'amazon_web_services',
+    icon: 'devicon-amazonwebservices-original' },
+  { name: 'stripe',
+    icon: 'fa-brands fa-stripe' }
 ]
 
 projects = [
   {
-    name: 'personal portfolio website',
+    name: 'Personal Portfolio Website',
     description: 'Website developed using pure HTML and CSS/Sass to highlight who I am and what projects I have in my portfolio. The aim of the website is to impress potential employers with the displayed portfolio.',
     link: 'github.com/rayves/myportfolio',
-    asset_id: [1, 2],
+    cover_path: assets[0],
+    asset_id: [2, 3, 4, 5],
     technology_id: [2, 3, 5, 7]
   },
   {
-    name: 'feed me',
+    name: 'Feed Me!',
     description: 'Command line application that is an easy-to-use point-of-sale program for the fictional fast-food restaurant “Feed Me!”',
     link: 'github.com/rayves/Feed-Me-terminal-application',
+    cover_path: assets[5],
+    asset_id: [7, 8, 9, 10],
     technology_id: [1]
   },
   {
-    name: 'roastxpress',
+    name: 'roastXpress',
     description: 'A simple two-way marketplace app between coffee buyers and suppliers that is easy to use. The site will allow suppliers/sellers an effective way to display their roastedcoffee to sell and for potential buyers to be able browse and purchase from the marketplace.',
     link: 'github.com/rayves/roastXpress',
-    asset_id: [3],
-    technology_id: [1, 2, 3, 4, 5, 7]
+    cover_path: assets[10],
+    asset_id: [12, 13, 14, 15],
+    technology_id: [2, 3, 7, 9, 1, 4, 8, 11, 12, 10]
   }
 ]
 
@@ -87,6 +113,7 @@ if Project.count == 0
       name: project[:name],
       description: project[:description],
       link: project[:link],
+      cover_path: project[:cover_path],
       asset_ids: project[:asset_id],
       technology_ids: project[:technology_id]
     )
